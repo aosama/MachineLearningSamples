a9fcb92f34abd4eecf53074702d368f1ef4186de
matthewgillett <msgille@gmail.com>
2018-11-28 15:11:17 -0500

    Updates to census income examples and added training data set.

--- src/main/scala/org/ibrahim/ezmachinelearning/DTCensusIncomeExample.scala
+++ src/main/scala/org/ibrahim/ezmachinelearning/DTCensusIncomeExample.scala
@@ -4,6 +4,7 @@ import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, DecisionTreeClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.{DataFrame, functions}
 
 object DTCensusIncomeExample extends SharedSparkContext {
@@ -25,52 +26,41 @@ object DTCensusIncomeExample extends SharedSparkContext {
       "hours-per-week",
       "native-country"
     )
-    val categoricalFields = Seq(1, 3, 5, 6, 7, 8, 9, 13)
-    val continuousFields = Seq(0, 2, 4, 10, 11, 12)
+    val categoricalFieldIndexes = Seq(1, 3, 5, 6, 7, 8, 9, 13)
+    val continuousFieldIndexes = Seq(0, 2, 4, 10, 11, 12)
 
-    // Create dataframe to hold census income data
+    // Create dataframes to hold census income data
     // Data retrieved from http://archive.ics.uci.edu/ml/datasets/Census+Income
-    var data = spark.read.format("csv").load("src/main/resources/adult.test")
+    var trainingData = spark.read.format("csv").load("src/main/resources/adult.data")
+    var testData = spark.read.format("csv").load("src/main/resources/adult.test")
 
-    // Trim leading spaces from data
-    for (colName <- data.columns)
-      data = data.withColumn(colName, functions.ltrim(functions.col(colName)))
-
-    for (i <- fields.indices)
-      data = data.withColumnRenamed("_c" + i, fields(i))
-
-    data = data.withColumnRenamed("_c14", "label")
+    // Format the data
+    trainingData = formatData(trainingData, fields, continuousFieldIndexes)
+    testData = formatData(testData, fields, continuousFieldIndexes)
 
     // Create object to convert categorical values to index values
     val categoricalIndexerArray =
-      for (i <- categoricalFields)
+      for (i <- categoricalFieldIndexes)
         yield new StringIndexer()
           .setInputCol(fields(i))
           .setOutputCol(fields(i) + "Indexed")
-
-    // Convert continuous values from string to double
-    for (i <- continuousFields) {
-      data = data.withColumn(fields(i), functions.col(fields(i)).cast("double"))
-    }
 
     // Create object to index label values
     val labelIndexer = new StringIndexer()
       .setInputCol("label")
       .setOutputCol("indexedLabel")
-      .fit(data)
+      .fit(trainingData)
 
     // Create object to generate feature vector from categorical and continuous values
     val vectorAssembler = new VectorAssembler()
-      .setInputCols((categoricalFields.map(i => fields(i) + "Indexed") ++ continuousFields.map(i => fields(i))).toArray)
+      .setInputCols((categoricalFieldIndexes.map(i => fields(i) + "Indexed") ++ continuousFieldIndexes.map(i => fields(i))).toArray)
       .setOutputCol("features")
 
-    // Split data into training and test data
-    val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2))
-
+    // Create decision tree
     val dt = new DecisionTreeClassifier()
       .setLabelCol("indexedLabel")
       .setFeaturesCol("features")
-      .setMaxBins(41) // Since feature "native-country" contains 41 distinct values, need to increase max bins.
+      .setMaxBins(42) // Since feature "native-country" contains 42 distinct values, need to increase max bins.
 
     // Create object to convert indexed labels back to actual labels for predictions
     val labelConverter = new IndexToString()
@@ -104,7 +94,7 @@ object DTCensusIncomeExample extends SharedSparkContext {
       .fit(trainingData)
       .transform(trainingData)
     indexedData.select("indexedLabel", "label").distinct().sort("indexedLabel").show()
-    showCategories(indexedData, fields, categoricalFields)
+    showCategories(indexedData, fields, categoricalFieldIndexes)
 
     val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("indexedLabel")
@@ -112,19 +102,50 @@ object DTCensusIncomeExample extends SharedSparkContext {
       .setMetricName("accuracy")
 
     val accuracy = evaluator.evaluate(predictions)
-    println(s"Test error = ${1.0 - accuracy}")
+    println(s"Test error = ${1.0 - accuracy}\n")
+
+    val metrics = new MulticlassMetrics(
+      predictions.select("indexedLabel", "prediction")
+      .rdd.map(x => (x.getDouble(0), x.getDouble(1)))
+    )
+
+    println(s"Confusion matrix:\n ${metrics.confusionMatrix}\n")
 
     val treeModel = model.stages(stageArray.length - 2).asInstanceOf[DecisionTreeClassificationModel]
 
     // Print out the tree with actual column names for features
     var treeModelString = treeModel.toDebugString
 
-    val featureFieldIndexes = categoricalFields ++ continuousFields
+    val featureFieldIndexes = categoricalFieldIndexes ++ continuousFieldIndexes
     for (i <- featureFieldIndexes.indices)
       treeModelString = treeModelString
         .replace("feature " + i + " ", fields(featureFieldIndexes(i)) + " ")
 
     println(s"Learned classification tree model:\n $treeModelString")
+  }
+
+  def formatData(df: DataFrame, fields: Seq[String], continuousFieldIndexes: Seq[Int]): DataFrame = {
+    var data = df
+
+    // Trim leading spaces from data
+    for (colName <- data.columns)
+      data = data.withColumn(colName, functions.ltrim(functions.col(colName)))
+
+    // Assign column names
+    for (i <- fields.indices)
+      data = data.withColumnRenamed("_c" + i, fields(i))
+
+    data = data.withColumnRenamed("_c14", "label")
+
+    // Convert continuous values from string to double
+    for (i <- continuousFieldIndexes) {
+      data = data.withColumn(fields(i), functions.col(fields(i)).cast("double"))
+    }
+
+    // Remove '.' character from label
+    data = data.withColumn("label", functions.regexp_replace(functions.col("label"), "\\.", ""))
+
+    data
   }
 
   def showCategories(df: DataFrame, fields: Seq[String], categoricalFieldIndexes: Seq[Int]): Unit = {
